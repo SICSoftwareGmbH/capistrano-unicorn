bd5a69281108550dbe4ce2c430ec40cf71439483
Florian Schwab <florian.schwab@sic-software.com>
2013-12-13 14:17:11 +0100

    fixed usage of sudo if unicorn_user is set

--- lib/capistrano/unicorn/utility.rb
+++ lib/capistrano/unicorn/utility.rb
@@ -40,7 +40,7 @@ module CapistranoUnicorn
     # Check if a remote process exists using its pid file
     #
     def remote_process_exists?(pid_file)
-      test("[ -e #{pid_file} ]") && execute("#{try_unicorn_user} kill -0 `cat #{pid_file}` > /dev/null 2>&1")
+      test("[ -e #{pid_file} ]") && execute(*try_unicorn_user, :kill, '-0', "`cat #{pid_file}`", '> /dev/null 2>&1', raise_on_non_zero_exit: false)
     end
 
     # Stale Unicorn process pid file
@@ -76,7 +76,7 @@ module CapistranoUnicorn
     # Send a signal to a unicorn master processes
     #
     def unicorn_send_signal(signal, pid=get_unicorn_pid)
-      execute try_unicorn_user, 'kill', '-s', signal, pid
+      execute *try_unicorn_user, 'kill', '-s', signal, pid
     end
 
     # Run a command as the :unicorn_user user if :unicorn_user is a string.
@@ -84,9 +84,9 @@ module CapistranoUnicorn
     #
     def try_unicorn_user
       if unicorn_user = fetch(:unicorn_user)
-        "sudo -u #{unicorn_user}"
+        [:sudo, '-u', unicorn_user]
       else
-        ''
+        nil
       end
     end
 
@@ -117,7 +117,7 @@ module CapistranoUnicorn
           puts 'Unicorn is already running!'
           return
         else
-          execute :rm, fetch(:unicorn_pid)
+          execute *try_unicorn_user, :rm, fetch(:unicorn_pid)
         end
       end
 
@@ -125,7 +125,7 @@ module CapistranoUnicorn
 
       within fetch(:app_path) do
         with rails_env: fetch(:rails_env), bundle_gemfile: fetch(:bundle_gemfile) do
-          execute :bundle, 'exec', fetch(:unicorn_bin), '-c', unicorn_config_file_path, '-E', fetch(:unicorn_rack_env), '-D', fetch(:unicorn_options)
+          execute *try_unicorn_user, fetch(:unicorn_bin), '-c', unicorn_config_file_path, '-E', fetch(:unicorn_rack_env), '-D', fetch(:unicorn_options)
         end
       end
     end
@@ -139,9 +139,7 @@ module CapistranoUnicorn
     end
 
     def unicorn_roles
-      # TODO proc necessary here?
-      Proc.new{ fetch(:unicorn_roles, :app) }.call
-      #defer{ fetch(:unicorn_roles, :app) }
+      fetch(:unicorn_roles, :app)
     end
   end
 end
